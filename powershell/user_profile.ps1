8981be895b08463871d2df6ea7771a772fef613a
AndreM222 <mossiroberto0392@gmail.com>
2023-05-07 18:48:04 -0600

    feat(powershell): shortcuts

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,15 +1,12 @@
-#Prompt
 
 oh-my-posh init pwsh --config $HOME\.config\powershell\themes\minimalNight.omp.json | Invoke-Expression
 Import-Module Terminal-Icons
 
 # PSReadLine
-
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
 # Alias
-
 Set-Alias clr clear
 Set-Alias vi nvim
 Set-Alias vim nvim
@@ -21,3 +18,6 @@ function servers { netstat -ano }
 function server([string]$SERVERNAME) { netstat -ano | findstr "$SERVERNAME"}
 function la {ls -force}
 function drives {gdr -PSProvider 'FileSystem'}
+function vdot { cd $HOME/Appdata/Local/nvim }
+function vpdot { cd $HOME/Appdata/Local/nvim/after/plugin }
+function vldot { cd $HOME/Appdata/Local/nvim/lua/andrem222 }
