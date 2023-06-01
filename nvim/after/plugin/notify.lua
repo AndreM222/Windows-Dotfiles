399490e6d2c04bc22898a711ec5e263b3551be00
Andre Mossi <98118238+AndreM222@users.noreply.github.com>
2023-06-01 10:55:01 -0600

    fix(spam notification): remove lsp notification to prevent spam from linter

--- nvim/after/plugin/notify.lua
+++ nvim/after/plugin/notify.lua
@@ -45,45 +45,6 @@ local function format_message(message, percentage)
     return (percentage and percentage .. "%\t" or "") .. (message or "")
 end
 
-
--- LSP integration
-vim.lsp.handlers["$/progress"] = function(_, result, ctx)
-    local val = result.value
-    local client_id = ctx.client_id
-
-    if not val.kind then return end
-
-    local notif_data = get_notif_data(client_id, result.token)
-
-    if val.kind == "begin" then
-        local message = format_message(val.message, val.percentage)
-
-        notif_data.notification = vim.notify(message, "info", {
-            title = format_title(val.title, vim.lsp.get_client_by_id(client_id).name),
-            icon = spinner_frames[1],
-            timeout = false,
-            hide_from_history = false,
-        })
-
-        notif_data.spinner = 1
-        update_spinner(client_id, result.token)
-    elseif val.kind == "report" and notif_data then
-        notif_data.notification = vim.notify(format_message(val.message, val.percentage), "info", {
-            replace = notif_data.notification,
-            hide_from_history = false
-        })
-    elseif val.kind == "end" and notif_data then
-        notif_data.notification =
-            vim.notify(val.message and format_message(val.message) or "Complete", "info", {
-                icon = "",
-                replace = notif_data.notification,
-                timeout = 3000
-            })
-
-        notif_data.spinner = nil
-    end
-end
-
 -- DAP integration
 
 dap.listeners.before['event_progressStart']['progress-notifications'] = function(session, body)
