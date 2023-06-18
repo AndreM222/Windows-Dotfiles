39be97e0d724fa56eed3cbdb804c0f0f5e990adc
Andre Mossi <98118238+AndreM222@users.noreply.github.com>
2023-06-18 17:19:13 -0600

    feat(posh git): add posh git

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -19,7 +19,8 @@ Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory
 
 $env:FZF_DEFAULT_OPTS="--color=fg:#f8f8f2,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
 
-# Env
+# Git
+Import-Module posh-git
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 
 # Alias
