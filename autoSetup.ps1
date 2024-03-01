895d9c4b0dea8309c59324c6ec8bff7baa300d9d
Andre Mossi <mossiroberto0392@gmail.com>
2024-02-29 21:45:39 -0500

    feat(simpligy setup): simplify by adding descriptions in list and automize selection with switch

--- autoSetup.ps1
+++ autoSetup.ps1
@@ -19,45 +19,41 @@ Import-Module ".\library.psm1"
 
 
 
-#region Package Manager Setup
-installerSearch "winget list --id" "Winget Install" $container["packManagers"]
-# $packManagers
+#region Setup Functions
+foreach($item in $container)
+{
+    Switch($item["INSTALL_TYPE"])
+    {
 
-installerExe "Invoke-RestMethod" @(,@("Scoop", "scoop", "get.scoop.sh"))
+        #region Executable Installer
+        "Executable"
+        { 
+            installerExe $item["MANAGER"] $item["CONTAINER"] 
+        }
+        #endregion Executable Installer
 
-installerExe "nvm install" $container["npmVersions"]
-#endregion Package Manager Setup
+        #region Search Installer
+        "Search"
+        { 
+            installerSearch $item["FINDER"] $item["MANAGER"] $item["CONTAINER"] 
+        }
+        #region Search Installer
 
+        #region Git Setups
+        "Git"
+        { 
+            gitRepoSetup $item["CONTAINER"] 
+        }
+        #endregion Git Dotfile Setups
 
+        #region Script Dotfile Setup
+        "Script"
+        { 
+            scriptSetup $item["CONTAINER"] 
+        }
+        #endregion Script Dotfile Setup
+    }
+}
+#region Setup Functions
 
-#region Setup Installations
-# -- Winget
-installerSearch "winget list --id" "Winget install" $container["winList"]
-
-# -- Scoop
-installerSearch "scoop list" "scoop install" $container["scoopPathList"]
-
-# -- NPM
-installerExe "npm install -g" $container["npmList"]
 Write-Output '{ "path": "cz-conventional-changelog" }' > ~/.czrc # Set Commitizen Path
-
-# -- Dotnet
-installerExe "dotnet tool install -g" $container["dotnetList"]
-
-# -- PIP
-installerExe "pip install --upgrade" $container["pipList"]
-
-# -- PowerShell
-installerSearch "Get-Module -ListAvailable" "Install-Module -Force" $container["powerList"]
-#endregion Setup Tools
-
-
-
-#region Git Setups
-gitRepoSetup $container["gitDotfileList"]
-#endregion Git Setups
-
-
-
-#region Terminal Config Setup
-scriptSetup $container["scriptDotfileList"]
--- listSetup.ps1
+++ listSetup.ps1
@@ -1,84 +1,148 @@
-@{
-    packManagers = @( # (Name, Id, Path)
-        # Package Managers <- Required
-        @("PowerShell Core", " Microsoft.PowerShell_8wekyb3d8bbwe", "Microsoft.Powershell"),
-        @("Git", "Git.Git", "Git.Git"),
-        @("NVM", "CoreyButler.NVMforWindows", "CoreyButler.NVMforWindows"),
-        @("Python3", "Python.Python.3.12", "Python3")
-    )
+@(
+    # Pre-Requisites
+    @{
+        TITLE = "Package Managers"
+        INSTALL_TYPE = "Search"
+        MANAGER = "Winget Install"
+        FINDER = "Winget List --id"
+        CONTAINER = @( # (Name, Id, Path)
+            # Package Managers <- Required
+            @("PowerShell Core", " Microsoft.PowerShell_8wekyb3d8bbwe", "Microsoft.Powershell"),
+            @("Git", "Git.Git", "Git.Git"),
+            @("NVM", "CoreyButler.NVMforWindows", "CoreyButler.NVMforWindows"),
+            @("Python3", "Python.Python.3.12", "Python3")
+        )
+    }
+    
+    @{
+        TITLE = "Node Versions"
+        INSTALL_TYPE = "Executable"
+        MANAGER = "NVM Install"
+        CONTAINER = @( # (Name, executable, Path)
+            # Node Versions <- Required
+            ,@("Node LTS", "nvm use lts", "lts")
+        )
+    }
 
-    npmVersions = @( # (Name, executable, Path)
-        # Node Versions <- Required
-        ,@("NPM LTS", "nvm use lts", "lts")
-    )
+    @{
+        TITLE = "Scoop Manager"
+        INSTALL_TYPE = "Executable"
+        MANAGER = "Invoke-RestMethod"
+        CONTAINER = @( # (Name, executable, Path)
+            # Package Manager <- Required
+            ,@("Scoop", "scoop", "get.scoop.sh")
+        )
+    }
 
     # Tools Installations
-    winList = @( # (Name, Id, Path) 
-        # Tools
-        @("SQLite", "SQLite.SQLite", "SQLite.SQLite"),
-        @("Neovim", "Neovim.Neovim", "Neovim"),
-        @("Oh My Posh", "JanDeDobbeleer.OhMyPosh", "JanDeDobbeleer.OhMyPosh -s winget"),
-        @("Yarn", "Yarn.Yarn", "Yarn.Yarn"),
-        @("Fuzzy Finder", "junegunn.fzf", "fzf"),
-        @("Bat", "sharkdp.bat", "sharkdp.bat"),
-        @("LazyDocker", "JesseDuffield.Lazydocker", "lazydocker"),
-        @("NTop", "gsass1.NTop", "ntop"),
-        @("RipGrep", "BurntSushi.ripgrep.MSVC", "BurntSushi.ripgrep.MSVC"),
-        @("LazyGit", "JesseDuffield.lazygit", "lazygit"),
-        # Server
-        @("Clangd", "LLVM.clangd", "clangd"),
-        @("Lua Language Server", "LuaLS.lua-language-server", "lua-language-server"),
-        @("LLVM", "LLVM.LLVM", "-i LLVM.LLVM")
-    )
+    @{
+        TITLE = "Winget Tools"
+        INSTALL_TYPE = "Search"
+        MANAGER = "Winget Install"
+        FINDER = "Winget List --id"
+        CONTAINER = @( # (Name, Id, Path) 
+            # Tools
+            @("SQLite", "SQLite.SQLite", "SQLite.SQLite"),
+            @("Neovim", "Neovim.Neovim", "Neovim"),
+            @("Oh My Posh", "JanDeDobbeleer.OhMyPosh", "JanDeDobbeleer.OhMyPosh -s winget"),
+            @("Yarn", "Yarn.Yarn", "Yarn.Yarn"),
+            @("Fuzzy Finder", "junegunn.fzf", "fzf"),
+            @("Bat", "sharkdp.bat", "sharkdp.bat"),
+            @("LazyDocker", "JesseDuffield.Lazydocker", "lazydocker"),
+            @("NTop", "gsass1.NTop", "ntop"),
+            @("RipGrep", "BurntSushi.ripgrep.MSVC", "BurntSushi.ripgrep.MSVC"),
+            @("LazyGit", "JesseDuffield.lazygit", "lazygit"),
+            # Server
+            @("Clangd", "LLVM.clangd", "clangd"),
+            @("Lua Language Server", "LuaLS.lua-language-server", "lua-language-server"),
+            @("LLVM", "LLVM.LLVM", "-i LLVM.LLVM")
+        )
+    }
+    
+    @{
+        TITLE = "NPM Tools"
+        INSTALL_TYPE = "Executable"
+        MANAGER = "NPM Install -g"
+        CONTAINER = @( # (Name, Executable, Path)
+            # Tools
+            @("Commitizen", "commitizen", "commitizen cz-conventional-changelog"),
+            @("Generate License", "generate", "generate generate-license"),
+            @("Npm Check Updates", "npm-check-updates", "npm-check-updates"),
+            # Server
+            @("Live Server", "live-server", "live-server"),
+            @("Pyright", "pyright", "pyright"),
+            @("Typescript Language Server", "typescript-language-server", "typescript typescript-language-server"),
+            @("Tailwindcss Language Server", "tailwindcss-language-server", "tailwindcss-language-server"),
+            # Formatter
+            @("prettier", "prettier", "prettier")
+        )
+    }
+    
+    @{
+        TITLE = "Dotnet Tools"
+        INSTALL_TYPE = "Executable"
+        MANAGER = "Dotnet Tool Install -g"
+        CONTAINER = @( # (Name, Executable, Path)
+            # Server
+            ,@("CSharp LS", "csharp-ls", "csharp-ls")
+        )
+    }
 
-    npmList = @( # (Name, Executable, Path)
-        # Tools
-        @("Commitizen", "commitizen", "commitizen cz-conventional-changelog"),
-        @("Generate License", "generate", "generate generate-license"),
-        @("Npm Check Updates", "npm-check-updates", "npm-check-updates"),
-        # Server
-        @("Live Server", "live-server", "live-server"),
-        @("Pyright", "pyright", "pyright"),
-        @("Typescript Language Server", "typescript-language-server", "typescript typescript-language-server"),
-        @("Tailwindcss Language Server", "tailwindcss-language-server", "tailwindcss-language-server"),
-        # Formatter
-        @("prettier", "prettier", "prettier")
-    )
-
-    dotnetList = @( # (Name, Executable, Path)
-        # Server
-        ,@("CSharp LS", "csharp-ls", "csharp-ls")
-    )
-
-    pipList = @( # (Name, Executable, Path)
-        # Formatter
-        ,@("Autopep8", "autopep8", "autopep8")
-    )
-
-    scoopPathList = @( # (Name, Id, Path)
-        # Tools
-        @("Fira Code Font", "FiraCode-NF-Mono", "nerd-fonts/FiraCode-NF-Mono"),
-        @("GCC", "gcc", "gcc")
-    )
-
-    powerList = @( # (Name, Id, Path)
-        # Tools
-        @("PSFzf", "PSFzf", "PSFzf"),
-        @("Posh Git", "posh-git", "posh-git -Scope CurrentUser"),
-        @("Terminal Icons", "Terminal-Icons", "Terminal-Icons -Repository PSGallery"),
-        @("PSReadline", "PSReadLine", "PSReadLine -AllowPrereleas"),
-        @("Z", "z", "z")
-    )
-
-    gitDotfileList = @( # (Name, Repo, Path)
-        # Dot Setup
-        @("PowerShell", "https://github.com/AndreM222/PowerShell.git", "Documents"),
-        @("nvim", "https://github.com/AndreM222/nvim.git", "AppData\Local")
-    )
-
-    scriptDotfileList = @( # (Path, File)
-        # Dot Setup
-        @("AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState", "settings.json"),
-        @(".", ".gitconfig")
-    )
-}
+    @{
+        TITLE = "Pip Tools"
+        INSTALL_TYPE = "Executable"
+        MANAGER = "Pip Install"
+        CONTAINER = @( # (Name, Executable, Path)
+            # Formatter
+            ,@("Autopep8", "autopep8", "autopep8")
+        )
+    }
+    
+    @{
+        TITLE = "Scoop Tools"
+        INSTALL_TYPE = "Search"
+        MANAGER = "Scoop Install"
+        FINDER = "Scoop List"
+        CONTAINER = @( # (Name, Id, Path)
+            # Tools
+            @("Fira Code Font", "FiraCode-NF-Mono", "nerd-fonts/FiraCode-NF-Mono"),
+            @("GCC", "gcc", "gcc")
+        )
+    }
+    
+    @{
+        TITLE = "PowerShell Tools"
+        INSTALL_TYPE = "Search"
+        MANAGER = "Install-Module -Force"
+        FINDER = "Get-Module -ListAvailable"
+        CONTAINER = @( # (Name, Id, Path)
+            # Tools
+            @("PSFzf", "PSFzf", "PSFzf"),
+            @("Posh Git", "posh-git", "posh-git -Scope CurrentUser"),
+            @("Terminal Icons", "Terminal-Icons", "Terminal-Icons -Repository PSGallery"),
+            @("PSReadline", "PSReadLine", "PSReadLine -AllowPrereleas"),
+            @("Z", "z", "z")
+        )
+    }
+    
+    # Dotfiles Setup
+    @{
+        TITLE = "Git Dotfiles"
+        INSTALL_TYPE = "Git"
+        CONTAINER = @( # (Name, Repo, Path)
+            # Dot Setup
+            @("PowerShell", "https://github.com/AndreM222/PowerShell.git", "Documents"),
+            @("nvim", "https://github.com/AndreM222/nvim.git", "AppData\Local")
+        )
+    }
+    
+    @{
+        TITLE = "Script Dotfiles"
+        INSTALL_TYPE = "Script"
+        CONTAINER = @( # (Path, File)
+            # Dot Setup
+            @("AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState", "settings.json"),
+            @(".", ".gitconfig")
+        )
+    }
+)
